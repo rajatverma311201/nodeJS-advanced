7b39ce0666273400b4fc42b448a13fca468a1c12
Rajat Verma <rajat311201@gmail.com>
2024-08-13 00:39:16 +0530

    events update

--- 01_events/my-custom-event-emitter.js
+++ 01_events/my-custom-event-emitter.js
@@ -19,6 +19,30 @@ class MyCustomEventEmitter {
             listener(...args);
         });
     }
+
+    once(event, listener) {
+        const onceListener = (...args) => {
+            listener(...args);
+            this.removeListener(event, onceListener);
+        };
+        this.on(event, onceListener);
+    }
+
+    // removeListener(event, listener) {
+    //     const availableListeners = this.events[event];
+    //     if (!availableListeners) {
+    //         return;
+    //     }
+
+    //     const index = availableListeners.indexOf(listener);
+    //     if (index !== -1) {
+    //         availableListeners.splice(index, 1);
+    //     }
+    // }
+
+    // removeAllListeners(event) {
+    //     delete this.events[event];
+    // }
 }
 
 module.exports = { MyCustomEventEmitter };
--- 01_events/test.js
+++ 01_events/test.js
@@ -10,6 +10,10 @@ myEvent.on("myEvent", (data) => {
     console.log("second myEvent  |  Event triggered with data:", data);
 });
 
+myEvent.once("once-event", (data) => {
+    console.log("once myEvent    |  Event triggered with data:", data);
+});
+
 myEvent.emit("myEvent", "outer");
 
 //they all will show at a gap of 1 second each
@@ -20,3 +24,9 @@ for (let i = 0; i < 5; i++) {
 
     setTimeout(handler, 1000 * i);
 }
+
+//this will show only once
+myEvent.emit("once-event", "outer");
+myEvent.emit("once-event", "outer");
+myEvent.emit("once-event", "outer");
+myEvent.emit("once-event", "outer");
